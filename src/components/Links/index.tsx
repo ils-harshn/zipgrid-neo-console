427e3e414b4114f084058b0a24804e19456176fb
ils-harshn <h.narwariya@thesynapses.com>
2023-12-07 10:27:03 +0530

    added link forgot password

--- src/components/Links/index.tsx
+++ src/components/Links/index.tsx
@@ -1,18 +1,19 @@
 import { Link } from "react-router-dom";
 import { AType } from "./links.type";
 import { getClassName } from "../../utils";
+import "./index.scss";
 
 const A: React.FC<AType> = ({
   children,
   className = "",
   size = "medium",
   varient = "primary",
-  underline = "underline",
+  underline = "underline-none",
   ...props
 }) => {
   return (
     <Link
-      className={getClassName(className, size, varient, underline)}
+    className={getClassName("link", className, size, varient, underline)}
       {...props}
     >
       {children}
