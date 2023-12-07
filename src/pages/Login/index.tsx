2afdcdac965a7c1a3db477518897386ab42127f3
ils-harshn <h.narwariya@thesynapses.com>
2023-12-07 11:51:50 +0530

    auto complete off

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -19,6 +19,7 @@ const Login: React.FC = () => {
         type="text"
         placeholder="Username"
         width="initial"
+        autoComplete="off"
       />
       <TextInput
         className="login-password"
