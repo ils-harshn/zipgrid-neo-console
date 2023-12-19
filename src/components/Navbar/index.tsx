a6ab606609c0f545f12401af23fafcca7ec6fc5c
ils-harshn <h.narwariya@thesynapses.com>
2023-12-19 15:34:58 +0530

    Completed navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,24 +1,70 @@
 import { HomeSvg } from "../../assets/svgs";
 import { getClassName } from "../../utils";
 import "./index.scss";
-import { NavbarType } from "./index.type";
+import { NavbarType, SubNavCallerType } from "./index.type";
+import { NavbarData } from "./NavbarData";
+
+const SubNavCaller: React.FC<SubNavCallerType> = ({
+  data,
+  className = "",
+  replace = false,
+}) => {
+  
+  const checkOverFlow = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    const nestedMenu = e.currentTarget.getElementsByClassName(
+      "recur-sub-nav-caller"
+    )[0] as HTMLElement;
+    if (nestedMenu) {
+      const dropdownRect = nestedMenu.getBoundingClientRect();
+      const viewportWidth = window.innerWidth;
+
+      if (dropdownRect.right > viewportWidth) {
+        nestedMenu.style.left = `unset`;
+        nestedMenu.style.right = `100%`;
+      }
+    }
+  };
+
+  return data ? (
+    <ul
+      className={getClassName(
+        replace ? className : "sub-nav-caller" + className
+      )}
+    >
+      {data.map((item, index) => (
+        <li
+          className="sub-nav-caller-tiles"
+          key={index}
+          onMouseEnter={checkOverFlow}
+        >
+          {item.label}
+          <hr />
+          <SubNavCaller
+            data={item.menuList}
+            className="recur-sub-nav-caller"
+            replace={true}
+          />
+        </li>
+      ))}
+    </ul>
+  ) : null;
+};
 
 const Navbar: React.FC<NavbarType> = ({ className }) => {
   return (
     <nav className={getClassName(className, "nav-header")}>
-      <ul>
-        <li>
+      <ul className="nav-header-ul-main">
+        <li className="nav-header-tiles">
           <HomeSvg />
+          <span className="nav-header-sep"></span>
         </li>
-        <li>Setup & Records</li>
-        <li>Social & Communications</li>
-        <li>Finance</li>
-        <li>Compliance</li>
-        <li>Desk</li>
-        <li>Club</li>
-        <li>Gate Management</li>
-        <li>Reports</li>
-        <li>Chats</li>
+        {NavbarData.map((item, index) => (
+          <li key={index} className="nav-header-tiles">
+            {item.label}
+            <span className="nav-header-sep" />
+            <SubNavCaller data={item.menuList} />
+          </li>
+        ))}
       </ul>
     </nav>
   );
